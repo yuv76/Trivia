7f1be0dc8a3f0fbe4c057090141aeefec7f4448f
gali hadar <galihadar1@gmail.com>
2024-04-16 16:44:31 +0300

    changing the close socket

--- client/main.py
+++ client/main.py
@@ -3,6 +3,7 @@ import socket
 SERVER_IP = "127.0.0.1"
 SERVER_PORT = 9090
 
+
 def main():
     # Create a TCP socket for the client
     server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -17,12 +18,18 @@ def main():
     client_msg = "Hello"
     try:
         server_sock.sendall(client_msg.encode())
-        server_sock.close()
 
     except ConnectionResetError as error:
         print("the connection was cut", error)
 
         return 0
+
+    # receive the server exit message
+    # server_msg = server_sock.recv(1024)
+    # server_msg = server_msg.decode()
+    # if client_msg == "EXIT":
+    #    print(server_msg)
+    #    server_sock.close()
 
 
 if __name__ == "__main__":
