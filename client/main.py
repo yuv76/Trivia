272ad82b731b0f35fa474c135bbd5a46966f11be
yuv76 <u.val.orian@gmail.com>
2024-04-16 15:10:38 +0300

    fixed client crashing problem

--- client/main.py
+++ client/main.py
@@ -4,26 +4,25 @@ SERVER_IP = "127.0.0.1"
 SERVER_PORT = 9090
 
 def main():
-    true = "true"
-    while true == "true":
-        # Create a TCP socket for the client
-        server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server_sock.connect((SERVER_IP, SERVER_PORT))
+    # Create a TCP socket for the client
+    server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_sock.connect((SERVER_IP, SERVER_PORT))
 
-        # receive the server message
-        server_msg = server_sock.recv(1024)
-        server_msg = server_msg.decode()
-        print(server_msg)
+    # receive the server message
+    server_msg = server_sock.recv(1024)
+    server_msg = server_msg.decode()
+    print(server_msg)
+
+    # send the clients message to the server
+    client_msg = "Hello"
+    try:
+        server_sock.sendall(client_msg.encode())
         server_sock.close()
 
-        # send the clients message to the server
-        client_msg = "Hello"
-        try:
-            server_sock.sendall(client_msg.encode())
+    except ConnectionResetError as error:
+        print("the connection was cut", error)
 
-        except ConnectionResetError as error:
-            print("the connection was cut", error)
-            return 0
+        return 0
 
 
 if __name__ == "__main__":
