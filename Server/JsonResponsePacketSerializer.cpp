943d59f3079d1fa1849820efd62236b8149e8456
yuv76 <u.val.orian@gmail.com>
2024-04-18 23:20:57 +0300

    added Json library

---
+++ Server/JsonResponsePacketSerializer.cpp
@@ -0,0 +1,16 @@
+#include "JsonResponsePacketSerializer.h"
+
+std::vector<std::string> JsonResponsePacketSerializer::serializeResponse(ErrorResponse err)
+{
+
+}
+
+std::vector<std::string> JsonResponsePacketSerializer::serializeResponse(LoginResponse log)
+{
+
+}
+
+std::vector<std::string> JsonResponsePacketSerializer::serializeResponse(SignupResponse sig)
+{
+
+}
