2faebb968788ddc61e8f6a682b23c09ee24b5029
yuv76 <u.val.orian@gmail.com>
2024-04-19 00:01:52 +0300

    JsonResponsePacketSerializer class

--- Server/JsonResponsePacketSerializer.h
+++ Server/JsonResponsePacketSerializer.h
@@ -5,13 +5,17 @@
 
 enum msgCodes
 {
-	ERROR = 0, 
+	ERR = 0,
 	SIGNUP,
 	LOGIN
 };
 
+#define BYTE 8
+#define INT_BYTES 4
+
 class JsonResponsePacketSerializer
 {
+public:
 	static std::vector<std::string> serializeResponse(ErrorResponse err);
 	static std::vector<std::string> serializeResponse(LoginResponse log);
 	static std::vector<std::string> serializeResponse(SignupResponse sig);
