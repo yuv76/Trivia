666bc52fcdeb5d5f413c968c9fb9616965e3e108
yuv76 <u.val.orian@gmail.com>
2024-04-24 12:26:52 +0300

    returned main to regular

--- Server/Main.cpp
+++ Server/Main.cpp
@@ -7,19 +7,9 @@ int main()
 {
 	try
 	{
-		/*
 		//run server.
 		Server server;
 		server.Run();
-		*/
-		LoginManager lm;
-		std::cout << lm.signup("Maria", "mrmrmr", "maria.h@hmail.com") << " "; // should be: 1
-		std::cout << lm.signup("Natasha123", "mrmrmr", "maria.h@hmail.com") << " "; // should be: 0
-		std::cout << lm.login("barnie", "barnn") << " "; // should be: -1
-		std::cout << lm.login("Natasha123", "wrong_password") << " ";; // should be: 0
-		std::cout << lm.login("Elirans", "eliran_t_best") << " ";; // should be: 1
-		std::cout << lm.logout("Maria") << " ";; // should be: 1
-		std::cout << lm.logout("gani") << " ";; // should be: 0
 	}
 	catch (std::exception& e)
 	{
