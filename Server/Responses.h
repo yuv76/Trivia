1a322bb2506eda85c832e7e1eebe0077cfb2fe64
yuv76 <u.val.orian@gmail.com>
2024-05-01 19:02:40 +0300

    added response structs

--- Server/Responses.h
+++ Server/Responses.h
@@ -20,3 +20,40 @@ public:
 	std::string message;
 };
 
+struct LogoutResponse
+{
+	unsigned int status;
+};
+
+struct GetRoomsResponse
+{
+	unsigned int status;
+	std::vector<RoomData> rooms;
+};
+
+struct GetPlayersInRoomResponse
+{
+	std::vector<std::string> players;
+};
+
+struct getHighScoreResponse
+{
+	unsigned int status;
+	std::vector<std::string> statistics;
+};
+
+struct getPersonalStatsResponse
+{
+	unsigned int status;
+	std::vector<std::string> statistics;
+};
+
+struct JoinRoomResponse
+{
+	unsigned int status;
+};
+
+struct CreateRoomResponse
+{
+	unsigned int status;
+};
