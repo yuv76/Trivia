68f5c37938f08745627c06d812b6b0fac2ea734b
yuv76 <u.val.orian@gmail.com>
2024-05-12 17:44:50 +0300

    added room manager

---
+++ Server/RoomManager.cpp
@@ -0,0 +1,69 @@
+#include "RoomManager.h"
+
+/*
+creates a new room.
+in: the user who creates the room, the room data.
+out: none.
+*/
+void RoomManager::createRoom(LoggedUser l, RoomData rd)
+{
+	Room newRoom(rd, l);
+	this->m_rooms.insert(std::make_pair(rd.id, newRoom));
+}
+
+/*
+deletes a room given its id.
+in: the room to delete's id.
+out: none.
+*/
+void RoomManager::deleteRoom(int ID)
+{
+	this->m_rooms.erase(ID);
+}
+
+/*
+gets room state (if active) according to its id.
+in: the room to get its state's id.
+out: the room's state.
+*/
+unsigned int RoomManager::getRoomState(int ID)
+{
+	if (this->m_rooms.find(ID) != this->m_rooms.end())
+	{
+		return this->m_rooms[ID].isActive();
+	}
+	return NO_STATE;
+
+}
+
+/*
+gets a vector with data of all rooms currently on the server.
+in: none.
+out: an std vector with every room's data.
+*/
+std::vector<RoomData> RoomManager::getRooms()
+{
+	std::vector<RoomData> roomDatas;
+
+	auto i = this->m_rooms.begin();
+	for (i; i != this->m_rooms.end(); i++)
+	{
+		roomDatas.push_back(i->second.getRoomData());
+	}
+
+	return roomDatas;
+}
+
+/*
+returns a room by its id.
+in: the id (int).
+out: the room itself (a reference).
+*/
+Room& RoomManager::getRoom(int ID)
+{
+	if (this->m_rooms.find(ID) != this->m_rooms.end()) // if key exists.
+	{
+		return this->m_rooms[ID];
+	}
+	throw std::exception("Invalid Room ID.");
+}
