cd566e16ae0ca231d70b7fa363562f8a83dfed2e
gali hadar <galihadar1@gmail.com>
2024-06-02 22:26:49 +0300

    adding the all users into the stats table

--- Client/CreateRoom.xaml.cs
+++ Client/CreateRoom.xaml.cs
@@ -121,7 +121,7 @@ namespace Client
 
         private void PutName()
         {
-            string temp = Communicator.getName();
+            string temp = "hello " + Communicator.getName();
             name.Text = temp;
         }
     }
--- Client/StatsMenu.xaml.cs
+++ Client/StatsMenu.xaml.cs
@@ -65,7 +65,7 @@ namespace Client
 
         private void PutName()
         {
-            string temp = Communicator.getName();
+            string temp = "hello " + Communicator.getName();
             name.Text = temp;
         }
     }
