797f5275da1048d86d7554d8b9f8193cc0d5d6e8
gali hadar <galihadar1@gmail.com>
2024-06-19 16:22:59 +0300

    adding the new question function in client

--- Client/AddQuestion.xaml.cs
+++ Client/AddQuestion.xaml.cs
@@ -1,13 +1,166 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using Responses;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Xml.Linq;
 
 namespace Client
 {
-    internal class AddQuestion
+    /// <summary>
+    /// Interaction logic for Signup.xaml
+    /// </summary>
+    public partial class AddQuestion : Window
     {
+        public bool _isClosedByX = true;
 
+        const string USERNAME_TAKEN = "This Username is already taken.\n";
+        const string PASSWORDS_NOT_MATCH = "Passwords dont match.\n";
+        const string INVALID_MAIL = "Invalid mail.\n";
+
+        public AddQuestion(double left, double top, double width, double height, WindowState windowstate)
+        {
+            /*
+            signup window C'tor.
+            in: the window's position (left, top, width, height, windowstate).
+            */
+
+            InitializeComponent();
+            Left = left;
+            Top = top;
+            Width = width;
+            Height = height;
+            WindowState = windowstate;
+
+            PutName();
+        }
+
+        private async void signupEnter_click(object sender, RoutedEventArgs e)
+        {
+            /*
+            event handler for the signup enter button, tries to prefom the signup request.
+            in: the sender, the event's arguments.
+            out: none.
+            */
+
+            string temp = NEWMAIL.Text, temp2;
+            int ok = 0;
+            string errors = "";
+            //check username existance with the server.
+
+            if (NEWSERNAME.Text.Length == 0)
+            {
+                ERRORS.Text = "Invalid username";
+            }
+            else
+            {
+                if (NEWPASS.Text != REPASS.Text)
+                {
+                    errors += PASSWORDS_NOT_MATCH;
+                }
+
+                //check if mail is in valid mail adress format.
+                string[] temp1 = temp.Split('@');
+                if (!NEWMAIL.Text.Contains('@'))
+                {
+                    errors += INVALID_MAIL;
+                }
+                else
+                {
+                    temp2 = temp1[1];
+                    string[] temp3 = temp2.Split('.');
+                    if (!temp2.Contains('.'))
+                    {
+                        errors += INVALID_MAIL;
+                    }
+                    else if (temp1[0].Length < 1)
+                    {
+                        errors += INVALID_MAIL;
+                    }
+                    else if (temp3[0].Length < 1)
+                    {
+                        errors += INVALID_MAIL;
+                    }
+                    else if (temp3[1].Length < 1)
+                    {
+                        errors += INVALID_MAIL;
+                    }
+                }
+
+                if (errors != "")
+                {
+                    ERRORS.Text = errors;
+                }
+                else
+                {
+                    ok = await Communicator.signupAsync(NEWSERNAME.Text, NEWPASS.Text, NEWMAIL.Text);
+                    if (ok == SignupResponse.SIGNUP_SUCCESS)
+                    {
+                        MainMenu men = new MainMenu(Left, Top, Width, Height, WindowState, "");
+                        men.Show();
+                        _isClosedByX = false;
+                        this.Close();
+                    }
+                    else if (ok == SignupResponse.SIGNUP_FAIL)
+                    {
+                        ERRORS.Text = "Username already taken";
+                    }
+                    else
+                    {
+                        ERRORS.Text = "Connection error.";
+                    }
+                }
+
+            }
+        }
+
+        private async void back_click(object sender, RoutedEventArgs e)
+        {
+            /*
+            event handler for the back button - returns to main menu.
+            in: the sender, the event's arguments.
+            out: none.
+            */
+
+            MainMenu sigi = new MainMenu(Left, Top, Width, Height, WindowState, "");
+            sigi.Show();
+            _isClosedByX = false;
+            this.Close();
+        }
+
+        protected override async void OnClosed(EventArgs e)
+        {
+            /*
+            event handler for closing window, seperates client closing it from closing it to move to another window.
+            in: the sender (Button), the event arguments.
+            out: none.
+            */
+
+            if (_isClosedByX)
+            {
+                int ok = await Communicator.signoutAsync();
+            }
+        }
+
+        private void PutName()
+        {
+            /*
+            displays name of connected user in the name textblock.
+            in&out: none.
+            */
+
+            string temp = "hello " + Communicator.getName();
+            name.Text = temp;
+        }
     }
 }
